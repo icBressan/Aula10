d10cf60f4d22f9707034e90fc6c1277384f2cfc3
Isabelle <ibressan@yahoo.com.br>
2024-11-07 12:22:00 -0300

    07/11 - Update index.js

--- servidor/index.js
+++ servidor/index.js
@@ -6,3 +6,21 @@ const port = 3001;
 
 app.use(cors());
 app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'mysqlfatec',
+    database: 'aulabd'
+});
+
+//conectando ao banco de dados
+
+db.connect((erro) => {
+    if (erro){
+        console.error('Erro ao conectar ao MySQL: ', erro);
+    } else {
+        console.log('Conectando ao MySQL com sucesso!');
+    }
+})
