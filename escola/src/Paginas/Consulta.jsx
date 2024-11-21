ac7d142b16f5445e578ca3dbe1f12b7a35311630
Isabelle <86361001+icBressan@users.noreply.github.com>
2024-11-21 12:14:19 -0300

    21/11 - Frontend Cadastro

--- escola/src/Paginas/Consulta.jsx
+++ escola/src/Paginas/Consulta.jsx
@@ -3,6 +3,15 @@ export default function Consulta()
     return (
         <div>
             <h1>Consulta</h1>
+            <table>
+                <tr> 
+                    <td> Codigo </td>
+                    <td> Nome </td>
+                    <td> Cidade </td>
+                    <td> Estado </td>
+                    <td> Opções </td>
+                </tr>
+            </table>
         </div>
     );
 }
